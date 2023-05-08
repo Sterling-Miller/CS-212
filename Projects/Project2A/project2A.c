4dfdd89c655ceefbd88a954691fc6d71778a7138
Sterling-Miller <stermiller11@gmail.com>
2023-05-08 12:34:31 -0700

    file

--- Projects/Project2A/project2A.c
+++ Projects/Project2A/project2A.c
@@ -2,10 +2,8 @@
 #include <stdio.h>
 
 int main() {
-    int i, j;
-
-    for (i = 10; i <= 500; i++) {
-        for (j = i - 1; j > 1; j--) {
+    for (int i = 10; i <= 500; i++) {
+        for (int j = i - 1; j > 1; j--) {
             if (i % j == 0 && i % 2 != 0) {
                 printf("%d is a composite number.\n", i);
                 break;
